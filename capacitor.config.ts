80f90fd7902bb535dc0bb7c3d297717c6005cf4d
Cesar Andriansyah <cesar.andriansyah98@gmail.com>
2021-11-11 10:16:06 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'get_goals',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
