53f0f3cbe6671d6dbaf7c34fce77ee6ef3f9d282
Cesar Andriansyah <cesar.andriansyah98@gmail.com>
2021-11-15 01:05:02 +0700

    first push

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonIcon, IonGrid, IonRow, IonCol} from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
@@ -7,15 +7,22 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Edit{}</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
+        <IonGrid>
+          <IonRow>
+            <IonCol class="ion-text-center">
+            <IonButton color="primary" >Primary</IonButton>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+        {/* <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">Blank</IonTitle>
           </IonToolbar>
-        </IonHeader>
+        </IonHeader> */}
         <ExploreContainer />
       </IonContent>
     </IonPage>
