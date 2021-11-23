22e60cfc1cce12edd31475299762006bb5a05b3b
Cesar Andriansyah <cesar.andriansyah98@gmail.com>
2021-11-24 00:25:58 +0700

    Create login and register function

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -1,86 +1,15 @@
-//Import Config
 import { initializeApp } from "firebase/app";
+import "firebase/compat/auth";
 
-//Import Auth
-import "firebase/compat/auth"
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth"
-import firebase from "firebase/compat/app"
-
-
-// SET UP CONFIG
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
-    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyBR26h7PnpydyPrATd0PlwnEjIcSDe0m-c",
+  authDomain: "get-goals-09031998.firebaseapp.com",
+  databaseURL: "https://get-goals-09031998.firebaseio.com",
+  projectId: "get-goals-09031998",
+  storageBucket: "get-goals-09031998.appspot.com",
+  messagingSenderId: "521644495951",
+  appId: "1:521644495951:web:e239d3066c6b235ac4c63a",
+  measurementId: "G-KN1MDQD4B4",
 };
 
-/* Version 9 */
-// const firebase = initializeApp(firebaseConfig);
-// export const auth = getAuth(firebase);
-
-/* Version 8 */
-if (!firebase.apps.length) firebase.initializeApp({...firebaseConfig})
-export const auth = firebase.auth()
-export const usersData = firebase.database().ref("users");
-
-// SET UP CREATE USER ACCOUNT
-// type userInfo = {
-//     name: string,
-//     userEmail: string,
-//     userPassword: string
-// }
-
-export const randomString = () => {
-    return (
-      Math.random().toString(36).substring(2, 15) +
-      Math.random().toString(36).substring(2, 15)
-    );
-  };
-
-export const createNewUser = (
-    user_uid: string,
-    user_email: string,
-    user_name: string,
-    pub_id: string
-) => {
-    usersData.child(user_uid).set({
-        id: user_uid,
-        email: user_email,
-        name: user_name,
-        public_id: pub_id,
-        points: 0
-    });
-};
-
-export const registerUser = async ( name:string, userEmail:string, userPassword:string ) => {  
-    /* Version 9 */
-    // await createUserWithEmailAndPassword(auth, userEmail, userPassword)
-    // .then((userCredential) => {
-    //     const user = userCredential.user;
-    // })
-    // .catch((error) =>{
-    //     const errorCode = error.code;
-    //     const errorMessage = error.message;
-    // })
-
-    /* Version 8 */
-    try{
-        await firebase.auth().createUserWithEmailAndPassword(userEmail,userPassword);
-
-        const user = firebase.auth().currentUser;
-        user?.updateProfile({
-            displayName: name,
-        });
-
-        if (user !== null){
-            const public_id = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-            
-        }
-    }
-
-}
+export const firebase = initializeApp(firebaseConfig);
---
+++ src/firebase/userFunction.ts
@@ -0,0 +1,45 @@
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { getDatabase, ref, set, onValue } from "firebase/database";
+import { firebase } from "./firebaseConfig";
+
+export const auth = getAuth(firebase);
+export const database = getDatabase();
+
+export const registerUser = async (
+  name: string,
+  userEmail: string,
+  userPassword: string
+) => {
+  await createUserWithEmailAndPassword(auth, userEmail, userPassword)
+    .then((userCredential: { user: any }) => {
+      const user = userCredential.user;
+
+      if (user !== null) {
+        set(ref(database, "users/" + user), {
+          username: name,
+          userEmail: userEmail,
+          userPassword: userPassword,
+          userCredential: user,
+        });
+      }
+    })
+    .catch((error: { code: any; message: any }) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    });
+};
+
+export const loginUser = async (userEmail: string, userPassword: string) => {
+  await signInWithEmailAndPassword(auth, userEmail, userPassword)
+    .then((userCredential: { user: any }) => {
+      const user = userCredential.user;
+    })
+    .catch((error: { code: any; message: any }) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    });
+};
